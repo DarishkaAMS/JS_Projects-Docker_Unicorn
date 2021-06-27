64c55dd179a4ca543da88304448804411c77b559
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-06-27 06:45:51 +0300

    Setup commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("<h2> Hi there =^_^= <h2>")
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`));
